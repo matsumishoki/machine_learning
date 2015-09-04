d302da24bf2ce4999b01ac02969372232217a05d
matsumi shoki <is0188ee@ed.ritsumei.ac.jp>
2015-09-04 15:53:44 +0900

    コードの見た目を綺麗にする

--- logistic_regression.py
+++ logistic_regression.py
@@ -30,36 +30,40 @@ if __name__ == '__main__':
     X = np.hstack((X_raw, np.ones((num_examples, 1))))
 
     # ρを定義する(ρ=0.1で良いか判断し，収束しなければ値を変える．)
-    rho = 0.01
+    rho = 1.4
 
     # wを定義する（65個の個数を持った配列をrandomで作成する）
     w = np.random.randn(65)
-#    w[-1] = 0
+
+    # 収束するまで繰り返す
+    max_iteration = 100
 
     # 確率的勾配降下法
-    for epoch in range(100):
+    for epoch in range(max_iteration):
         for x_i, t_i in zip(X, t):
             y_i = sigmoid(np.inner(w, x_i))
             w_new = w - rho * (y_i - t_i) * x_i
             w = w_new
 
-#        e_i = -(t_i*(np.log(y_i)) + (1 - t_i)*np.log(1 - y_i))
-#        print "t_i:", t_i, ",y_i:", y_i, "np.inner(w, x_i):", np.inner(w, x_i)
-#        print "    E_i:", e_i, ", |w|:", np.linalg.norm(w)
-#        assert not (np.isnan(e_i) or np.isinf(e_i)), "e_i == {}".format(e_i)
-
+        # 負の対数尤度関数の値を表示する
         y = sigmoid(np.inner(w, X))
         error = np.sum(-(t*(np.log(y)) + (1 - t)*np.log(1 - y)))
-        print "error:", error
+        # print "error:", error
 
         # 正解クラスと予測クラスとの比較
         predict_class = y >= 0.5
         num_correct = np.sum(t == predict_class)
         correct_percent = num_correct / float(num_examples) * 100
         print "correct_percent:", correct_percent
+        if correct_percent == 100.0:
+            break
+
+    # 予測クラスと真のクラスを表示する
+    print "predict_class:", predict_class
+    print "t:", t == 1
 
     # wの可視化
     w_t = w[0:64]
+    print "w_t:", w_t
     plt.matshow(w_t.reshape(8, 8), cmap=plt.cm.gray)
     plt.show()
-    print "w_t:", w_t

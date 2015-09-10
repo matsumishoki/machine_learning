d88ca8487acaa6b5196ef7eb0c559299f1018068
matsumi shoki <is0188ee@ed.ritsumei.ac.jp>
2015-09-10 13:59:26 +0900

    ソフトマックス回帰のスケッチを追加する

--- softmax_regression.py
+++ softmax_regression.py
@@ -5,3 +5,46 @@ Created on Fri Sep 04 17:07:32 2015
 @author: matsumi
 """
 
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.datasets import load_digits
+
+
+def softmax(s):
+    exp_s = np.exp(s)
+    return exp_s / np.sum(exp_s)
+
+
+# main文
+if __name__ == '__main__':
+    digits = load_digits()
+    images = digits.images
+    plt.matshow(images[4], cmap=plt.cm.gray)
+    plt.show()
+
+    # データ・セットの読み込み
+    X_raw = digits.data / 16.0
+    t = digits.target
+    num_examples = len(X_raw)
+    x = X_raw[0]
+    X = np.hstack((X_raw, np.ones((num_examples, 1))))
+
+    # ρを定義する(ρ=0.1で良いか判断し，収束しなければ値を変える．)
+
+
+    # wを10(0～9)クラス定義する（65個の個数を持った配列をrandomで作成する）
+
+
+    # 確率的勾配降下法
+
+
+        # 負の対数尤度関数の値を表示する
+
+
+        # 正解クラスと予測クラスとの比較
+
+
+    # 予測クラスと真のクラスを表示する
+
+
+    # wの可視化

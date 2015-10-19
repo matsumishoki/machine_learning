5b40ddc1eca10d6d059be8b17c966b4951cbce34
matsumi shoki <is0188ee@ed.ritsumei.ac.jp>
2015-10-19 21:42:06 +0900

    X_trainをminibatchSGDで取り出す

--- test_reading_train.py
+++ test_reading_train.py
@@ -10,10 +10,19 @@ import matplotlib.pyplot as plt
 from sklearn.datasets import load_digits
 
 
-def onehot(k, num_classes=10):
-    num_examples = len(k)
-    t_onehot = np.zeros((num_examples, num_classes))
-    t_onehot[np.arange(num_examples), k] = 1
-    return t_onehot
-
 X_train = np.arange(10*3).reshape((10, 3))
+num_train = len(X_train)
+batch_size = 3
+num_batches = num_train / batch_size
+max_iteration = 2
+
+for epoch in range(max_iteration):
+    print "epoch:", epoch
+    perm = np.random.permutation(num_train)
+    for batch_indexes in np.array_split(perm, num_batches):
+        X_batch = X_train[batch_indexes]
+
+        print "batch_indexes:", batch_indexes
+        print "X_batch:"
+        print X_batch
+        print

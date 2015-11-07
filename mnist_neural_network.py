cbc65ab98d3736a4612844c633614441963ff0e2
matsumi shoki <is0188ee@ed.ritsumei.ac.jp>
2015-11-07 19:20:50 +0900

    MNISTデータセットを読み込む

--- mnist_neural_network.py
+++ mnist_neural_network.py
@@ -5,3 +5,42 @@ Created on Sat Nov 07 19:09:38 2015
 @author: matsumi
 """
 
+import load_mnist
+import numpy as np
+from sklearn.cross_validation import train_test_split
+import matplotlib.pyplot as plt
+
+
+if __name__ == '__main__':
+    x_train, t_train, x_test, t_test = load_mnist.load_mnist()
+    t_train = t_train.astype(np.int32)
+    t_test = t_test.astype(np.int32)
+    plt.matshow(x_train[0].reshape(28, 28), cmap=plt.cm.gray)
+    plt.show()
+
+    print "x_train.shape:", x_train.shape
+    print "t_train.shape:", t_train.shape
+
+    # 60000ある訓練データセットを50000と10000の評価のデータセットに分割する
+    x_train, x_valid, t_train, t_valid = train_test_split(
+        x_train, t_train, test_size=0.1, random_state=100)
+
+    print "x_train.shape:", x_train.shape
+    print "t_train.shape:", t_train.shape
+    print "x_valid.shape:", x_valid.shape
+    print "t_valid.shape:", t_valid.shape
+    print "x_test.shape:", x_test.shape
+    print "t_test.shape:", t_test.shape
+
+    num_train = len(x_train)
+    num_valid = len(x_valid)
+    num_test = len(x_test)
+
+    # wxの中に定数項であるバイアス項を入れ込む
+    x_train = np.hstack((x_train, np.ones((num_train, 1))))
+    x_valid = np.hstack((x_valid, np.ones((num_valid, 1))))
+    x_test = np.hstack((x_test, np.ones((num_test, 1))))
+
+    classes = np.unique(t_train)  # 定義されたクラスラベル
+    num_classes = len(classes)  # クラス数
+    dim_features = x_train.shape[-1]  # xの次元

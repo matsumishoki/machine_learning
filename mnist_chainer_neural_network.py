6eed486d4d106816c82c7c4041a4c02e98a5f3c0
matsumi shoki <is0188ee@ed.ritsumei.ac.jp>
2015-12-06 17:45:00 +0900

    chainerでニューラルネットワークが動くようにする

--- mnist_chainer_neural_network.py
+++ mnist_chainer_neural_network.py
@@ -16,58 +16,19 @@ from chainer import Variable, FunctionSet
 from chainer.optimizers import SGD, Adam
 
 
-def softmax(s):
-    len(s.shape)
-    exp_s = np.exp(s)
-    if len(s.shape) == 1:
-        return exp_s / np.sum(exp_s)
+def error_and_accuracy(model, x_data, t_data):
+    x = Variable(x_data)
+    t = Variable(t_data)
 
-    if len(s.shape) == 2:
-        return exp_s / np.sum(exp_s, axis=1, keepdims=True)
+    a_1 = linear_1(x)
+    z_1 = F.relu(a_1)
+    z_1 = F.dropout(z_1, 0.5)
 
+    a_y = linear_2(z_1)
 
-def onehot(k, num_classes=10):
-    assert isinstance(k, np.ndarray)
-    assert k.ndim == 1
-    assert k.dtype == np.int, k.dtype
-
-    num_examples = len(k)
-    t_onehot = np.zeros((num_examples, num_classes))
-    t_onehot[np.arange(num_examples), k] = 1
-    return t_onehot
-
-
-# PRML pp209 l1 交差エントロピー誤差と正解率を求める関数
-def error_and_accuracy(w_1, w_2, x, t):
-    # 交差エントロピー誤差を計算する
-    # 順伝播
-    # 入力層と中間層のx(1×D)w_1(D×M)によって訓練データとの行列積(xw_1)を計算する(a_j(1×M)を求める)
-    a_j = np.dot(x, w_1)
-
-    # 求まったa_j(1×M)を隠れユニットのz(1×M)にする(活性化関数にa_j(1×M)を代入する)
-    z = np.tanh(a_j)
-
-    # zのshapeを変更する
-    z_new_shape = np.hstack((z, np.ones((len(z), 1))))
-
-    # 入力されたz(1×M)と,中間層と入力層のw_2(M×K)によって行列積(zw_2)を計算する(a_y(1×K))
-    a_y = np.dot(z_new_shape, w_2)
-
-    # 出力a_y(1×K)をsoftmax関数に代入するy(1×K)
-    y = softmax(a_y)
-    T = onehot(t)
-    num_examples = len(x)
-    error = np.sum(-(T*(np.log(y)))) / num_examples
-    assert not np.any(np.isnan(error))
-    assert not np.any(np.isinf(error))
-
-    # 正解率を計算する
-    predict_class = np.argmax(y, axis=1)
-    num_correct = np.sum(t == predict_class)
-    accuracy = num_correct / float(num_examples) * 100
-
-    return (error, accuracy)
-
+    error = F.softmax_cross_entropy(a_y, t)
+    accuracy = F.accuracy(a_y, t)
+    return error.data, accuracy.data * 100
 
 if __name__ == '__main__':
     x_train, t_train, x_test, t_test = load_mnist.load_mnist()
@@ -94,10 +55,10 @@ if __name__ == '__main__':
     num_valid = len(x_valid)
     num_test = len(x_test)
 
-    # wxの中に定数項であるバイアス項を入れ込む
-    x_train = np.hstack((x_train, np.ones((num_train, 1))))  # (N×D)
-    x_valid = np.hstack((x_valid, np.ones((num_valid, 1))))
-    x_test = np.hstack((x_test, np.ones((num_test, 1))))
+#    # wxの中に定数項であるバイアス項を入れ込む
+#    x_train = np.hstack((x_train, np.ones((num_train, 1))))  # (N×D)
+#    x_valid = np.hstack((x_valid, np.ones((num_valid, 1))))
+#    x_test = np.hstack((x_test, np.ones((num_test, 1))))
 
     classes = np.unique(t_train)  # 定義されたクラスラベル
     num_classes = len(classes)  # クラス数
@@ -110,12 +71,20 @@ if __name__ == '__main__':
     batch_size = 300       # ミニバッチ1つあたりのサンプル数
     dim_hidden = 100         # 隠れ層の次元数を定義する
 
-    # dim_features次元の重みをnum_classesクラス分用意する
-    # 入力層と中間層の間のw_1(D×M)
-    w_1 = w_scale * np.random.randn(dim_features, dim_hidden)
+#    # dim_features次元の重みをnum_classesクラス分用意する
+#    # 入力層と中間層の間のw_1(D×M)
+#    w_1 = w_scale * np.random.randn(dim_features, dim_hidden)
+#
+#    # 中間層と出力層の間のw(M×K)
+#    w_2 = w_scale * np.random.randn(dim_hidden+1, num_classes)
 
-    # 中間層と出力層の間のw(M×K)
-    w_2 = w_scale * np.random.randn(dim_hidden+1, num_classes)
+    linear_1 = F.Linear(dim_features, dim_hidden)
+    linear_2 = F.Linear(dim_hidden, num_classes)
+    model = FunctionSet(linear_1=linear_1,
+                        linear_2=linear_2)
+#    optimizer = SGD(lr=learning_rate)
+    optimizer = Adam(learning_rate)
+    optimizer.setup(model)
 
     error_history = []
     train_accuracy_history = []
@@ -140,47 +109,21 @@ if __name__ == '__main__':
         for batch_indexes in np.array_split(perm, num_batches):
             x_batch = x_train[batch_indexes]
             t_batch = t_train[batch_indexes]
-            T_batch = onehot(t_batch)
 
-            # 順伝播
-            # 入力層と中間層のx(1×D)w_1(D×M)によって訓練データとの行列積(xw_1)を計算する(a_j(1×M)を求める)
-            a_j = np.dot(x_batch, w_1)
+            x = Variable(x_batch)
+            t = Variable(t_batch)
 
-            # 求まったa_j(1×M)を隠れユニットのz(1×M)にする(活性化関数にa_j(1×M)を代入する)
-            z = np.tanh(a_j)
+            a_1 = linear_1(x)
+            z_1 = F.relu(a_1)
+            z_1 = F.dropout(z_1, 0.5)
 
-            # zのshapeを変更する
-            z_new_shape = np.hstack((z, np.ones((len(z), 1))))
+            a_y = linear_2(z_1)
+            error = F.softmax_cross_entropy(a_y, t)
+            accuracy = F.accuracy(a_y, t)
 
-            # 入力されたz(1×M)と,中間層と入力層のw_2(M×K)によって行列積(zw_2)を計算する(a_y(1×K))
-            a_y = np.dot(z_new_shape, w_2)
-
-            # 出力a_y(1×K)をsoftmax関数に代入するy(1×K)
-            y = softmax(a_y)
-
-            # 逆伝播
-            # 出力された値y(1×K)から正解ラベルt(1×K)を引く(y-t)(δ_y(1×K))
-            d_y = y - T_batch
-
-            # z.T(M×1)とδ_y(1×K)との行列積(z.T δ_y)を計算する
-            # (誤差をw_2で微分したもの(grad_w_2(M×K)))
-            grad_w_2 = np.dot(z_new_shape.T, d_y)
-
-            # δ_y(1×K)とw_2(K×M)との行列積(δ_y w2)を計算する(grad_z(1×M))
-            grad_z = np.dot(d_y, w_2.T)
-
-            # grad_z(1×M)と(1-z**2)の要素積を計算する(δ_z(1×M))
-            d_z = grad_z * (np.ones((batch_size,
-                                     dim_hidden+1)) - z_new_shape**2)
-
-            # x.T(D×1)とδ_z(1×M)との行列積(x.T δ_z)を計算する(grad_w_1(D×M))
-            grad_w_1 = np.dot(x_batch.T, d_z)
-
-            # w_1を更新する(w_1 = w_1 - learning_rate*grad_w_1)
-            w_1 = w_1 - learning_rate * grad_w_1[:, :-1]
-
-            # w_2を更新する(w_2 = w_2 - learning_rate*grad_w_2)
-            w_2 = w_2 - learning_rate * grad_w_2
+            optimizer.zero_grads()
+            error.backward()
+            optimizer.update()
 
         time_finish = time.time()
         time_elapsed = time_finish - time_start
@@ -190,7 +133,7 @@ if __name__ == '__main__':
         # E(K×K)を出す0.5×(y-t)×(y-t).T次元数は，{0.5×(1×K)(K×1)}
         # E = sum(t×log(y)(1×K))
         # 訓練データセットの交差エントロピー誤差と正解率を表示する
-        train_error, train_accuracy = error_and_accuracy(w_1, w_2,
+        train_error, train_accuracy = error_and_accuracy(model,
                                                          x_train, t_train)
         print "[train] Error:", train_error
         print "[train] Accuracy:", train_accuracy
@@ -198,7 +141,7 @@ if __name__ == '__main__':
         train_accuracy_history.append(train_accuracy)
 
         # 検証データセットの交差エントロピー誤差と正解率を表示する
-        valid_error, valid_accuracy = error_and_accuracy(w_1, w_2,
+        valid_error, valid_accuracy = error_and_accuracy(model,
                                                          x_valid, t_valid)
         print "[valid] Error:", valid_error
         print "[valid] Accuracy:", valid_accuracy
@@ -222,8 +165,7 @@ if __name__ == '__main__':
 
         # 検証データの誤差が良ければwの最善値を保存する
         if valid_error <= valid_error_best:
-            w_1_best = w_1
-            w_2_best = w_2
+            model_best = copy.deepcopy(model)
             epoch_best = epoch
             valid_error_best = valid_error
             valid_accuracy_best = valid_accuracy
@@ -233,7 +175,7 @@ if __name__ == '__main__':
             print
 
     # 学習済みのモデルをテストセットで誤差と正解率を求める
-    test_error, test_accuracy = error_and_accuracy(w_1, w_2,
+    test_error, test_accuracy = error_and_accuracy(model,
                                                    x_test, t_test)
 
     print "[test]  Accuracy:", test_accuracy
@@ -247,18 +189,16 @@ if __name__ == '__main__':
     print
 
     # wの可視化
-    print "|w_1_best|:", np.linalg.norm(w_1_best)
+#    print "|w_1_best|:", np.linalg.norm(w_1_best)
     print "w_1_best:", w_1_best
-    print "|w_2_best|:", np.linalg.norm(w_2_best)
+#    print "|w_2_best|:", np.linalg.norm(w_2_best)
     print "w_2_best:", w_2_best
-    w_best = np.dot(w_1_best, w_2_best[:-1, :])
-    w_best = w_best.T
-    fig, axes = plt.subplots(2, 5,  figsize=(10, 4))
-    for w_k, ax in zip(w_best, axes.ravel()):
-        w_true = w_k[0:784]  # w_trueとは結果をプロットするために定義したものである
-        ax.matshow(w_true.reshape(28, 28), cmap=plt.cm.gray)
-    plt.show()
-    print "|w_2_best|:", np.linalg.norm(w_2_best)
-    print "w_2_best:", w_2_best
-
-
+#    w_best = np.dot(w_1_best, w_2_best[:-1, :])
+#    w_best = w_best.T
+#    fig, axes = plt.subplots(2, 5,  figsize=(10, 4))
+#    for w_k, ax in zip(w_best, axes.ravel()):
+#        w_true = w_k[0:784]  # w_trueとは結果をプロットするために定義したものである
+#        ax.matshow(w_true.reshape(28, 28), cmap=plt.cm.gray)
+#    plt.show()
+#    print "|w_2_best|:", np.linalg.norm(w_2_best)
+#    print "w_2_best:", w_2_best

f62b1661d7650fa195a9f3944b222731c144bf5a
matsumi shoki <is0188ee@ed.ritsumei.ac.jp>
2016-05-11 16:02:31 +0900

    knnに必要な関数名を定義する

--- digits_k-nn.py
+++ digits_k-nn.py
@@ -10,44 +10,28 @@ import matplotlib.pyplot as plt
 from sklearn.datasets import load_digits
 from sklearn.neighbors import KNeighborsClassifier
 
+
 class K_NN(object):
     def __init__(self, learning_rate=0.5, max_iteration=100):
-        self.learning_rate = learning_rate
-        self.max_iteration = max_iteration
+        pass
 
     def fit(self, X, y):
-        dimension = X.shape[1]
-        w = np.random.randn(dimension)
-        y = self._convert_label(y)
-        for i in range(self.max_iteration):
+        pass
 
-            for (x_i, y_i) in zip(X, y):
-                g_i = np.inner(w, x_i)
+    def Kneighbors(self, X, n_neighbors=None, return_distance=True):
+        pass
 
-                if g_i * y_i < 0:
-                    w = w + self.learning_rate * y_i * x_i
-
-        # wをインスタンス変数として持たせる
-        self.w = w
+    def Kneighbors_graph(self, X=None, n_neighbors=None):
+        pass
 
     def predict(self, X):
-        y = np.sign(np.inner(self.w, X))
-        return self._revert_label(y)
+        pass
+
+    def predict_proba(self, X):
+        pass
 
     def score(self, X, y):
-        predict_y = self.predict(X)
-        correct_rate = np.mean(predict_y == y)
-        return correct_rate
-
-    def _convert_label(self, y):
-        y = y.copy()
-        y[y == 0] = -1
-        return y
-
-    def _revert_label(self, y):
-        y = y.copy()
-        y[y == -1] = 0
-        return y
+        pass
 
 if __name__ == '__main__':
 
@@ -58,10 +42,6 @@ if __name__ == '__main__':
 
     # ライブラリを使用しない時
     classifier = K_NN()
-    classifier.fit(X, T)
-    y = classifier.predict(X)
-    print "y:", y
-    print "classifier_accuracy:", classifier.score(X, T)
 
     # ライブラリを使用した時
     lib_classifier = KNeighborsClassifier()

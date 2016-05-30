b20c2d5b75f137d0be5a22c7fe1526d4518e1201
matsumi shoki <is0188ee@ed.ritsumei.ac.jp>
2016-05-30 16:07:58 +0900

    線形回帰による線形関数のフィッティングのファイルを追加する

---
+++ linear_regression.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May 30 16:00:22 2016
+
+@author: matsumi
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import  Axes3D
+
+
+if __name__ == '__main__':
+    # 訓練データをロード
